b157f65e1affac126ddebdf25cdc19b47d49e81e
courtieu <courtieu@c0685fd8-6de5-4121-bad2-4297bca5a80f>
2009-01-18 04:53:19 +0000

    ajout d'une petite fonction qui parcours une preuve. A modifier pour avoir des verif interessantes.
    git-svn-id: svn+ssh://cplex.ensiie.fr/home/julien.forest/svnpub/ill_coq@44 c0685fd8-6de5-4121-bad2-4297bca5a80f

--- emma.v
+++ emma.v
@@ -86,10 +86,45 @@ Proof with try solve [ apply Id;reflexivity | prove_multiset_eq].
   bang_d (V ⊸ A)...(* !D au lieu de WL *)
   weak_impl_l V A...
   apply Oplus_R_1...
-Qed.
+Defined.
+
+(*
+Set Printing Depth 50000.
+Print originelle.
+*)
+
+Inductive boolP : Prop := trueb : boolP | falseb: boolP.
+
+Require Import JMeq.
+
+Program Fixpoint essai (e:env) (f:formula) (h: e ⊢ f) {struct h}: boolP := 
+match h with
+  | Id p => trueb
+  | Impl_R Γ p q x => essai _ _ x
+  | Impl_L Γ Δ p q r x x0 => if essai _ _ x then trueb else essai _ _ x0
+  | Times_R Γ Δ p q x x0 => falseb
+  | Times_L Γ p q r x => falseb
+  | One_R => falseb
+  | One_L Γ p x => essai _ _ x
+  | And_R Γ p q x x0 => falseb
+  | And_L_1 Γ p q r x => essai _ _ x
+  | And_L_2 Γ p q r x => essai _ _ x
+  | Oplus_L Γ p q r x x0 => if essai _ _ x then trueb else essai _ _ x0
+  | Oplus_R_1 Γ p q x => essai _ _ x
+  | Oplus_R_2 Γ p q x => essai _ _ x
+  | T_ Γ => falseb
+  | Zero_ Γ p x => falseb
+  | Bang_D Γ p q x => essai _ _ x
+  | Bang_C Γ p q x => essai _ _ x
+  | Bang_W Γ p q x => falseb
+  | Multiset Γ Γ' p x x0 => essai _ _ x0
+end.
+
+Eval vm_compute in (essai _ _ originelle).
 
 
-Lemma marchepas :
+
+Lemma marchepas : 
   ({P&1, R, G, B&1, !(V⊸A), (E⊸A)&1, (P⊸M)&1,(R⊸1)&(R⊸E), (G⊸1)⊕(G⊸V), 1⊕((B⊸V)&(B⊸1))  } ⊢ A ⊕ M ).
 Proof with try solve [ apply Id;reflexivity | prove_multiset_eq].
   and_l_2 (R ⊸ 1) (R ⊸ E).
